2fc09c22537b680e39c6bcaafd927898cdb1ec09
Andrew Dassonville <dassonville.andrew@gmail.com>
2023-03-21 17:46:54 +0000

    Update dependencies

--- es5-autogenerated/index.js
+++ es5-autogenerated/index.js
@@ -1,33 +1,29 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var path = require('path');
-
 var Prerenderer = require('@prerenderer/prerenderer');
-
 var PuppeteerRenderer = require('@prerenderer/renderer-puppeteer');
-
 var _require = require('html-minifier'),
-    minify = _require.minify;
-
+  minify = _require.minify;
 function PrerenderSPAPlugin() {
   var _this = this;
-
   var rendererOptions = {}; // Primarily for backwards-compatibility.
 
-  this._options = {}; // Normal args object.
+  this._options = {};
 
+  // Normal args object.
   for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
     args[_key] = arguments[_key];
   }
-
   if (args.length === 1) {
-    this._options = args[0] || {}; // Backwards-compatibility with v2
+    this._options = args[0] || {};
+
+    // Backwards-compatibility with v2
   } else {
     console.warn("[prerender-spa-plugin] You appear to be using the v2 argument-based configuration options. It's recommended that you migrate to the clearer object-based configuration system.\nCheck the documentation for more information.");
     var staticDir, routes;
@@ -36,39 +32,34 @@ function PrerenderSPAPlugin() {
     });
     staticDir ? this._options.staticDir = staticDir : null;
     routes ? this._options.routes = routes : null;
-  } // Backwards compatiblity with v2.
+  }
 
-
+  // Backwards compatiblity with v2.
   if (this._options.captureAfterDocumentEvent) {
     console.warn('[prerender-spa-plugin] captureAfterDocumentEvent has been renamed to renderAfterDocumentEvent and should be moved to the renderer options.');
     rendererOptions.renderAfterDocumentEvent = this._options.captureAfterDocumentEvent;
   }
-
   if (this._options.captureAfterElementExists) {
     console.warn('[prerender-spa-plugin] captureAfterElementExists has been renamed to renderAfterElementExists and should be moved to the renderer options.');
     rendererOptions.renderAfterElementExists = this._options.captureAfterElementExists;
   }
-
   if (this._options.captureAfterTime) {
     console.warn('[prerender-spa-plugin] captureAfterTime has been renamed to renderAfterTime and should be moved to the renderer options.');
     rendererOptions.renderAfterTime = this._options.captureAfterTime;
   }
-
   this._options.server = this._options.server || {};
   this._options.renderer = this._options.renderer || new PuppeteerRenderer(Object.assign({}, {
     headless: true
   }, rendererOptions));
-
   if (this._options.postProcessHtml) {
     console.warn('[prerender-spa-plugin] postProcessHtml should be migrated to postProcess! Consult the documentation for more information.');
   }
 }
-
 PrerenderSPAPlugin.prototype.apply = function (compiler) {
   var _this2 = this;
+  var compilerFS = compiler.outputFileSystem;
 
-  var compilerFS = compiler.outputFileSystem; // From https://github.com/ahmadnassri/mkdirp-promise/blob/master/lib/index.js
-
+  // From https://github.com/ahmadnassri/mkdirp-promise/blob/master/lib/index.js
   var mkdirp = function mkdirp(dir, opts) {
     return new Promise(function (resolve, reject) {
       try {
@@ -85,43 +76,44 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
       }
     });
   };
-
   var afterEmit = function afterEmit(compilation, done) {
     var PrerendererInstance = new Prerenderer(_this2._options);
     PrerendererInstance.initialize().then(function () {
       return PrerendererInstance.renderRoutes(_this2._options.routes || []);
-    }) // Backwards-compatibility with v2 (postprocessHTML should be migrated to postProcess)
+    })
+    // Backwards-compatibility with v2 (postprocessHTML should be migrated to postProcess)
     .then(function (renderedRoutes) {
       return _this2._options.postProcessHtml ? renderedRoutes.map(function (renderedRoute) {
         var processed = _this2._options.postProcessHtml(renderedRoute);
-
         if (typeof processed === 'string') renderedRoute.html = processed;else renderedRoute = processed;
         return renderedRoute;
       }) : renderedRoutes;
-    }) // Run postProcess hooks.
+    })
+    // Run postProcess hooks.
     .then(function (renderedRoutes) {
       return _this2._options.postProcess ? Promise.all(renderedRoutes.map(function (renderedRoute) {
         return _this2._options.postProcess(renderedRoute);
       })) : renderedRoutes;
-    }) // Check to ensure postProcess hooks returned the renderedRoute object properly.
+    })
+    // Check to ensure postProcess hooks returned the renderedRoute object properly.
     .then(function (renderedRoutes) {
       var isValid = renderedRoutes.every(function (r) {
         return typeof r === 'object';
       });
-
       if (!isValid) {
         throw new Error('[prerender-spa-plugin] Rendered routes are empty, did you forget to return the `context` object in postProcess?');
       }
-
       return renderedRoutes;
-    }) // Minify html files if specified in config.
+    })
+    // Minify html files if specified in config.
     .then(function (renderedRoutes) {
       if (!_this2._options.minify) return renderedRoutes;
       renderedRoutes.forEach(function (route) {
         route.html = minify(route.html, _this2._options.minify);
       });
       return renderedRoutes;
-    }) // Calculate outputPath if it hasn't been set already.
+    })
+    // Calculate outputPath if it hasn't been set already.
     .then(function (renderedRoutes) {
       renderedRoutes.forEach(function (rendered) {
         if (!rendered.outputPath) {
@@ -129,7 +121,8 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
         }
       });
       return renderedRoutes;
-    }) // Create dirs and write prerendered files.
+    })
+    // Create dirs and write prerendered files.
     .then(function (processedRoutes) {
       var promises = Promise.all(processedRoutes.map(function (processedRoute) {
         return mkdirp(path.dirname(processedRoute.outputPath)).then(function () {
@@ -144,7 +137,6 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
           if (typeof err === 'string') {
             err = `[prerender-spa-plugin] Unable to create directory ${path.dirname(processedRoute.outputPath)} for route ${processedRoute.route}. \n ${err}`;
           }
-
           throw err;
         });
       }));
@@ -160,7 +152,6 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
       done();
     });
   };
-
   if (compiler.hooks) {
     var plugin = {
       name: 'PrerenderSPAPlugin'
@@ -170,6 +161,5 @@ PrerenderSPAPlugin.prototype.apply = function (compiler) {
     compiler.plugin('after-emit', afterEmit);
   }
 };
-
 PrerenderSPAPlugin.PuppeteerRenderer = PuppeteerRenderer;
 module.exports = PrerenderSPAPlugin;
